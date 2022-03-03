4b5c0f266fc467a6c9de94a237b14f76de126956
Duyi Li <li.duy@northeastern.edu>
2022-03-02 17:08:45 -0800

    Initial Commit

---
+++ Stock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock
+//
+//  Created by Duyi Li on 3/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
